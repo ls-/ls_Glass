cd51fa05ba7ae6f1c859320453457580f8537a24
Val Voronov <i.lightspark@gmail.com>
2022-10-26 04:15:18 +0700

    Disable almost everything
    I need to debug this b 😁

--- ls_Glass/modules/uimanager.lua
+++ ls_Glass/modules/uimanager.lua
@@ -15,105 +15,107 @@ local CreateSlidingMessageFramePool = Core.Components.CreateSlidingMessageFrameP
 ----
 -- UIManager Module
 function UIManager:OnInitialize()
-  self.state = {
-    frames = {},
-    tabs = {},
-    temporaryFrames = {},
-    temporaryTabs = {}
-  }
+	self.state = {
+		frames = {},
+		tabs = {},
+		temporaryFrames = {},
+		temporaryTabs = {}
+	}
 end
 
 function UIManager:OnEnable()
-  self.tickerFrame = CreateFrame("Frame", "GlassUpdaterFrame", UIParent)
+	-- Mover
+	-- TODO: Replace with a stripped down version of LST or LSUI movers
+	self.moverFrame = CreateMoverFrame("GlassMoverFrame", UIParent)
+	-- self.moverDialog = CreateMoverDialog("GlassMoverDialog", UIParent)
 
-  -- Mover
-  self.moverFrame = CreateMoverFrame("GlassMoverFrame", UIParent)
-  self.moverDialog = CreateMoverDialog("GlassMoverDialog", UIParent)
+	-- Main Container
+	self.container = CreateMainContainerFrame("GlassFrame", UIParent)
+	self.container:SetPoint("TOPLEFT", self.moverFrame)
 
-  -- Main Container
-  self.container = CreateMainContainerFrame("GlassFrame", UIParent)
-  self.container:SetPoint("TOPLEFT", self.moverFrame)
+	-- Chat dock
+	-- self.dock = CreateChatDock(self.container)
 
-  -- Chat dock
-  self.dock = CreateChatDock(self.container)
+	-- SlidingMessageFrames
+	self.slidingMessageFramePool = CreateSlidingMessageFramePool(self.container)
 
-  -- SlidingMessageFrames
-  self.slidingMessageFramePool = CreateSlidingMessageFramePool(self.container)
+	for i=1, NUM_CHAT_WINDOWS do
+		local chatFrame = _G["ChatFrame"..i]
+		local smf = self.slidingMessageFramePool:Acquire()
+		smf:Init(chatFrame)
 
-  for i=1, NUM_CHAT_WINDOWS do
-    local chatFrame = _G["ChatFrame"..i]
-    local smf = self.slidingMessageFramePool:Acquire()
-    smf:Init(chatFrame)
+		self.state.frames[i] = smf
+		self.state.tabs[i] = CreateChatTab(smf)
+	end
 
-    self.state.frames[i] = smf
-    self.state.tabs[i] = CreateChatTab(smf)
-  end
+	-- -- Edit box
+	-- self.editBox = CreateEditBox(self.container)
 
-  -- Edit box
-  self.editBox = CreateEditBox(self.container)
+	-- -- Fix Battle.net Toast frame position
+	-- BNToastFrame:ClearAllPoints()
+	-- BNToastFrame:SetPoint("BOTTOMLEFT", ChatAlertFrame, "BOTTOMLEFT", 0, 0)
 
-  -- Fix Battle.net Toast frame position
-  BNToastFrame:ClearAllPoints()
-  BNToastFrame:SetPoint("BOTTOMLEFT", ChatAlertFrame, "BOTTOMLEFT", 0, 0)
+	-- ChatAlertFrame:ClearAllPoints()
+	-- ChatAlertFrame:SetPoint("BOTTOMLEFT", self.container, "TOPLEFT", 15, 10)
 
-  ChatAlertFrame:ClearAllPoints()
-  ChatAlertFrame:SetPoint("BOTTOMLEFT", self.container, "TOPLEFT", 15, 10)
+	-- -- Hide other chat elements
+	-- if Constants.ENV == "retail" then
+	--   QuickJoinToastButton:Hide()
+	-- end
 
-  -- Hide other chat elements
-  if Constants.ENV == "retail" then
-    QuickJoinToastButton:Hide()
-  end
+	-- ChatFrameChannelButton:Hide()
+	-- ChatFrameMenuButton:Hide()
 
-  ChatFrameChannelButton:Hide()
-  ChatFrameMenuButton:Hide()
+	-- -- Force classic chat style
+	-- if GetCVar("chatStyle") ~= "classic" then
+	--   SetCVar("chatStyle", "classic")
+	--   E.notify('Chat Style set to "Classic Style"')
 
-  -- Force classic chat style
-  if GetCVar("chatStyle") ~= "classic" then
-    SetCVar("chatStyle", "classic")
-    E.notify('Chat Style set to "Classic Style"')
+	--   -- Resets the background that IM style causes
+	--   self.editBox:SetFocus()
+	--   self.editBox:ClearFocus()
+	-- end
 
-    -- Resets the background that IM style causes
-    self.editBox:SetFocus()
-    self.editBox:ClearFocus()
-  end
+	-- -- Handle temporary chat frames (whisper popout, pet battle)
+	-- self:RawHook("FCF_OpenTemporaryWindow", function (...)
+	--   local chatFrame = self.hooks["FCF_OpenTemporaryWindow"](...)
+	--   local smf = self.slidingMessageFramePool:Acquire()
+	--   smf:Init(chatFrame)
 
-  -- Handle temporary chat frames (whisper popout, pet battle)
-  self:RawHook("FCF_OpenTemporaryWindow", function (...)
-    local chatFrame = self.hooks["FCF_OpenTemporaryWindow"](...)
-    local smf = self.slidingMessageFramePool:Acquire()
-    smf:Init(chatFrame)
+	--   self.state.temporaryFrames[chatFrame:GetName()] = smf
+	--   self.state.temporaryTabs[chatFrame:GetName()] = CreateChatTab(smf)
+	--   return chatFrame
+	-- end, true)
 
-    self.state.temporaryFrames[chatFrame:GetName()] = smf
-    self.state.temporaryTabs[chatFrame:GetName()] = CreateChatTab(smf)
-    return chatFrame
-  end, true)
+	-- -- Close window
+	-- self:RawHook("FCF_Close", function (chatFrame)
+	--   self.hooks["FCF_Close"](chatFrame)
 
-  -- Close window
-  self:RawHook("FCF_Close", function (chatFrame)
-    self.hooks["FCF_Close"](chatFrame)
+	--   self.slidingMessageFramePool:Release(self.state.temporaryFrames[chatFrame:GetName()])
+	--   self.state.temporaryFrames[chatFrame:GetName()] = nil
+	--   self.state.temporaryTabs[chatFrame:GetName()] = nil
+	-- end, true)
 
-    self.slidingMessageFramePool:Release(self.state.temporaryFrames[chatFrame:GetName()])
-    self.state.temporaryFrames[chatFrame:GetName()] = nil
-    self.state.temporaryTabs[chatFrame:GetName()] = nil
-  end, true)
+	-- Start rendering
+	-- TODO: Consider moving it elsewhere
+	self.tickerFrame = CreateFrame("Frame", "GlassUpdaterFrame", UIParent)
 
-  -- Start rendering
-  self.timeElapsed = 0
-  self.tickerFrame:SetScript("OnUpdate", function (_, elapsed)
-    self.timeElapsed = self.timeElapsed + elapsed
+	self.timeElapsed = 0
+	self.tickerFrame:SetScript("OnUpdate", function (_, elapsed)
+		self.timeElapsed = self.timeElapsed + elapsed
 
-    while (self.timeElapsed > 0.01) do
-      self.timeElapsed = self.timeElapsed - 0.01
+		while (self.timeElapsed > 0.01) do
+			self.timeElapsed = self.timeElapsed - 0.01
 
-      self.container:OnFrame()
+			self.container:OnFrame()
 
-      for _, smf in ipairs(self.state.frames) do
-        smf:OnFrame()
-      end
+			for _, smf in ipairs(self.state.frames) do
+				smf:OnFrame()
+			end
 
-      for _, smf in pairs(self.state.temporaryFrames) do
-        smf:OnFrame()
-      end
-    end
-  end)
+			for _, smf in pairs(self.state.temporaryFrames) do
+				smf:OnFrame()
+			end
+		end
+	end)
 end
