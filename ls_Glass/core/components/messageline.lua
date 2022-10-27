377f01e42174e9334e29dbb72016a26ac42ab27b
Val Voronov <i.lightspark@gmail.com>
2022-10-28 00:36:02 +0700

    Partially rewrite messageline
    I still need to rewrite that fader

--- ls_Glass/core/components/messageline.lua
+++ ls_Glass/core/components/messageline.lua
@@ -1,9 +1,7 @@
-local addonName, ns = ...
+local _, ns = ...
 local E, C, D, L = ns.E, ns.C, ns.D, ns.L
 
-local Core, Constants = unpack(select(2, ...))
-
-local Colors = Constants.COLORS
+local _, Constants = unpack(select(2, ...))
 
 local HyperlinkClick = Constants.ACTIONS.HyperlinkClick
 local HyperlinkEnter = Constants.ACTIONS.HyperlinkEnter
@@ -11,106 +9,82 @@ local HyperlinkLeave = Constants.ACTIONS.HyperlinkLeave
 
 local UPDATE_CONFIG = Constants.EVENTS.UPDATE_CONFIG
 
--- luacheck: push ignore 113
-local CreateFrame = CreateFrame
-local CreateObjectPool = CreateObjectPool
-local Mixin = Mixin
--- luacheck: pop
+local message_line_proto = {}
 
-local MessageLineMixin = {}
+do
+	function message_line_proto:SetIndentedWordWrap()
+		self.Text:SetIndentedWordWrap(C.db.profile.indented_word_wrap)
+	end
 
-function MessageLineMixin:Init()
-  self:SetWidth(Core.db.profile.frameWidth)
-  self:SetFadeInDuration(Core.db.profile.chatFadeInDuration)
-  self:SetFadeOutDuration(Core.db.profile.chatFadeOutDuration)
+	function message_line_proto:SetText(text, r, g, b, a)
+		self.Text:SetText(text)
+		self.Text:SetTextColor(r or 1, g or 1, b or 1, a)
 
-  local rightBgWidth = math.min(250, Core.db.profile.frameWidth - 50)
-  self:SetGradientBackground(50, rightBgWidth, Colors.codGray, Core.db.profile.chatBackgroundOpacity)
+		self:SetHeight(self.Text:GetStringHeight() + C.db.profile.chat.padding * 2)
+	end
 
-  if self.text == nil then
-    self.text = self:CreateFontString(nil, "ARTWORK", "GlassMessageFont")
-  end
-  self.text:SetPoint("LEFT", Constants.TEXT_XPADDING, 0)
-  self.text:SetWidth(Core.db.profile.frameWidth - Constants.TEXT_XPADDING * 2)
-  self.text:SetIndentedWordWrap(Core.db.profile.indentWordWrap)
+	function message_line_proto:UpdateGradient()
+		self:SetGradientBackgroundSize(50, math.min(250, C.db.profile.width - 50))
+		self:SetGradientBackgroundColor(0, 0, 0, C.db.profile.chat.opacity) -- TODO: Add me to config!
+	end
 
-  -- Hyperlink handling
-  self:SetHyperlinksEnabled(true)
+	function message_line_proto:OnHyperlinkClick(link, text, button)
+		E:Dispatch(HyperlinkClick({link, text, button}))
+	end
 
-  self:SetScript("OnHyperlinkClick", function (_, link, text, button)
-    E:Dispatch(HyperlinkClick({link, text, button}))
-  end)
+	function message_line_proto:OnHyperlinkEnter(link, text)
+		if C.db.profile.mouseover_tooltips then
+			E:Dispatch(HyperlinkEnter({link, text}))
+		end
+	end
 
-  self:SetScript("OnHyperlinkEnter", function (_, link, text)
-    if Core.db.profile.mouseOverTooltips then
-      E:Dispatch(HyperlinkEnter({link, text}))
-    end
-  end)
-
-  self:SetScript("OnHyperlinkLeave", function (_, link)
-    E:Dispatch(HyperlinkLeave(link))
-  end)
-
-  if self.subscriptions == nil then
-    self.subscriptions = {
-      E:Subscribe(UPDATE_CONFIG, function (key)
-        if key == "chatFadeInDuration" then
-          self:SetFadeInDuration(Core.db.profile.chatFadeInDuration)
-        end
-
-        if key == "chatFadeOutDuration" then
-          self:SetFadeOutDuration(Core.db.profile.chatFadeOutDuration)
-        end
-      end)
-    }
-  end
+	function message_line_proto:OnHyperlinkLeave(link)
+		E:Dispatch(HyperlinkLeave(link))
+	end
 end
 
----
--- Update height based on text height
-function MessageLineMixin:UpdateFrame()
-  local Ypadding = self.text:GetLineHeight() * Core.db.profile.messageLinePadding
-  local messageLineHeight = (self.text:GetStringHeight() + Ypadding * 2)
-  self:SetHeight(messageLineHeight)
 
-  self:SetWidth(Core.db.profile.frameWidth)
-  self.text:SetWidth(Core.db.profile.frameWidth - Constants.TEXT_XPADDING * 2)
-  self.text:SetIndentedWordWrap(Core.db.profile.indentWordWrap)
+local function createMessageLine(parent)
+	local frame = Mixin(CreateFrame("Frame", nil, parent), message_line_proto)
+	frame:SetWidth(C.db.profile.width)
+	frame:SetHyperlinksEnabled(true)
+	frame:SetScript("OnHyperlinkClick", frame.OnHyperlinkClick)
+	frame:SetScript("OnHyperlinkEnter", frame.OnHyperlinkEnter)
+	frame:SetScript("OnHyperlinkLeave", frame.OnHyperlinkLeave)
+	frame:Hide()
 
-  local rightBgWidth = math.min(250, Core.db.profile.frameWidth - 50)
-  self:SetGradientBackground(50, rightBgWidth, Colors.codGray, Core.db.profile.chatBackgroundOpacity)
+	-- frame:SetFadeInDuration(Core.db.profile.chatFadeInDuration) -- ! FadingFrameMixin
+	-- frame:SetFadeOutDuration(Core.db.profile.chatFadeOutDuration) -- ! FadingFrameMixin
+
+	E:CreateGradientBackground(frame, 50, math.min(250, C.db.profile.width - 50), 0, 0, 0, C.db.profile.chat.opacity) -- TODO: Add me to config!
+
+	frame.Text = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+	-- frame.Text = frame:CreateFontString(nil, "ARTWORK", "GlassMessageFont")
+	frame.Text:SetPoint("TOPLEFT", Constants.TEXT_XPADDING, 0)
+	frame.Text:SetPoint("BOTTOMRIGHT", -Constants.TEXT_XPADDING, 0)
+	frame.Text:SetJustifyH("LEFT")
+	frame.Text:SetJustifyV("MIDDLE")
+	frame.Text:SetIndentedWordWrap(C.db.profile.indented_word_wrap)
+
+	E:Subscribe(UPDATE_CONFIG, function (key)
+		if key == "chatFadeInDuration" then
+			-- frame:SetFadeInDuration(Core.db.profile.chatFadeInDuration) -- ! FadingFrameMixin
+		end
+
+		if key == "chatFadeOutDuration" then
+			-- frame:SetFadeOutDuration(Core.db.profile.chatFadeOutDuration) -- ! FadingFrameMixin
+		end
+	end)
+
+	return frame
 end
 
----
--- Update texture color based on setting
-function MessageLineMixin:UpdateTextures()
-  local rightBgWidth = math.min(250, Core.db.profile.frameWidth - 50)
-  self:SetGradientBackground(50, rightBgWidth, Colors.codGray, Core.db.profile.chatBackgroundOpacity)
+local function resetMessageLine(_, messageLine)
+	messageLine:ClearAllPoints()
+	messageLine:SetText("")
+	messageLine:Hide()
 end
 
-local function CreateMessageLine(parent)
-  local FadingFrameMixin = Core.Components.FadingFrameMixin
-  local GradientBackgroundMixin = Core.Components.GradientBackgroundMixin
-
-  local frame = CreateFrame("Frame", nil, parent)
-  local object = Mixin(frame, FadingFrameMixin, GradientBackgroundMixin, MessageLineMixin)
-
-  FadingFrameMixin.Init(object)
-  GradientBackgroundMixin.Init(object)
-  MessageLineMixin.Init(object)
-
-  return object
+function E:CreateMessageLinePool(parent)
+	return CreateObjectPool(function() return createMessageLine(parent) end, resetMessageLine)
 end
-
-local function CreateMessageLinePool(parent)
-  return CreateObjectPool(
-    function () return CreateMessageLine(parent) end,
-    function (_, message)
-      -- Reset all animations and timers
-      message:QuickHide()
-    end
-  )
-end
-
-Core.Components.CreateMessageLine = CreateMessageLine
-Core.Components.CreateMessageLinePool = CreateMessageLinePool
