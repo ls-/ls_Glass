a49219eb24bb48852ea309ea42c22b825eef3e4d
Val Voronov <i.lightspark@gmail.com>
2022-11-01 20:59:33 +0700

    Reset fading on show

--- ls_Glass/core/components/slidingmessageframe.lua
+++ ls_Glass/core/components/slidingmessageframe.lua
@@ -32,7 +32,7 @@ local function chatFrame_ShowHook(self)
 
 	if self.SlidingMessageFrame then
 		self.SlidingMessageFrame:Show()
-		self.SlidingMessageFrame:ScrollTo(0)
+		self.SlidingMessageFrame:ScrollTo(0, true)
 	end
 end
 
