1fb5108af2e691a55981e68fd370cd787af0c0ec
Val Voronov <i.lightspark@gmail.com>
2022-11-05 01:21:57 +0700

    🧹

--- ls_Glass/core/components/messageline.lua
+++ ls_Glass/core/components/messageline.lua
@@ -3,12 +3,6 @@ local E, C, D, L = ns.E, ns.C, ns.D, ns.L
 
 local _, Constants = unpack(select(2, ...))
 
-local HyperlinkClick = Constants.ACTIONS.HyperlinkClick
-local HyperlinkEnter = Constants.ACTIONS.HyperlinkEnter
-local HyperlinkLeave = Constants.ACTIONS.HyperlinkLeave
-
-local UPDATE_CONFIG = Constants.EVENTS.UPDATE_CONFIG
-
 local message_line_proto = {}
 
 do
@@ -28,38 +22,17 @@ do
 		self:SetGradientBackgroundSize(50, math.min(250, C.db.profile.width - 50))
 		self:SetGradientBackgroundColor(0, 0, 0, C.db.profile.chat.opacity) -- TODO: Add me to config!
 	end
-
-	-- function message_line_proto:OnHyperlinkClick(link, text, button)
-	-- -- function message_line_proto:OnHyperlinkClick(link, text, button)
-	-- 	print(link)
-	-- 	ChatFrame_OnHyperlinkShow(link, text, button)
-	-- 	-- SetItemRef(link, text, button)
-	-- 	-- E:Dispatch(HyperlinkClick({link, text, button}))
-	-- end
-
-	-- function message_line_proto:OnHyperlinkEnter(link, text)
-	-- 	if C.db.profile.mouseover_tooltips then
-	-- 		E:Dispatch(HyperlinkEnter({link, text}))
-	-- 	end
-	-- end
-
-	-- function message_line_proto:OnHyperlinkLeave(link)
-	-- 	E:Dispatch(HyperlinkLeave(link))
-	-- end
 end
 
 local function createMessageLine(parent)
 	local frame = Mixin(CreateFrame("Frame", nil, parent, "LSGlassHyperlinkPropagator"), message_line_proto)
 	frame:SetSize(C.db.profile.width, C.db.profile.chat.size + C.db.profile.chat.padding * 2)
-	-- frame:SetHyperlinksEnabled(true)
-	-- frame:SetScript("OnHyperlinkClick", frame.OnHyperlinkClick)
-	-- frame:SetScript("OnHyperlinkEnter", frame.OnHyperlinkEnter)
-	-- frame:SetScript("OnHyperlinkLeave", frame.OnHyperlinkLeave)
+	frame:SetAlpha(0)
 	frame:Hide()
 
 	E:CreateGradientBackground(frame, 50, math.min(250, C.db.profile.width - 50), 0, 0, 0, C.db.profile.chat.opacity) -- TODO: Add me to config!
 
-	frame.Text = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+	frame.Text = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal") -- TODO: Fix me!
 	-- frame.Text = frame:CreateFontString(nil, "ARTWORK", "GlassMessageFont")
 	frame.Text:SetPoint("LEFT", Constants.TEXT_XPADDING, 0)
 	frame.Text:SetPoint("RIGHT", -Constants.TEXT_XPADDING, 0)
@@ -74,7 +47,7 @@ local function resetMessageLine(_, messageLine)
 	messageLine.Text:SetText("")
 	messageLine:ClearAllPoints()
 	messageLine:Hide()
-	E:StopFading(messageLine, 1)
+	E:StopFading(messageLine, 0)
 end
 
 function E:CreateMessageLinePool(parent)
