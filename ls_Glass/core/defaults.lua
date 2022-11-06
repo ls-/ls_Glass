89262408fd1410cb0c31f7adda36d760a40d1640
Val Voronov <i.lightspark@gmail.com>
2022-11-06 22:38:47 +0700

    Remove config

--- ls_Glass/core/defaults.lua
+++ ls_Glass/core/defaults.lua
@@ -50,39 +50,4 @@ D.profile = {
 			out_duration = 0.6,
 		},
 	},
-
-	-- OLD
-	-- General
-	fontFlags = "",
-	frameWidth = 450,
-	frameHeight = 230,
-	positionAnchor = {
-		point = "BOTTOMLEFT",
-		xOfs = 20,
-		yOfs = 230
-	},
-
-	-- Edit box
-	editBoxFontSize = 12,
-	editBoxBackgroundOpacity = 0.6,
-	editBoxAnchor = {
-		position = "BELOW",
-		yOfs = -5
-	},
-
-	-- Messages
-	messageFontSize = 12,
-	chatBackgroundOpacity = 0.4,
-	messageLeading = 3,
-	messageLinePadding = 0.25,
-
-	chatHoldTime = 10,
-	chatShowOnMouseOver = true,
-	chatFadeInDuration = 0.6,
-	chatFadeOutDuration = 0.6,
-	chatSlideInDuration = 0.3,
-
-	indentWordWrap = true,
-	mouseOverTooltips = true,
-	iconTextureYOffset = 4,
 }
