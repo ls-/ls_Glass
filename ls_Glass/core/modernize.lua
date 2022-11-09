a007a74dbb53fa080ede21c4433922971f1ad20d
Val Voronov <i.lightspark@gmail.com>
2022-11-10 05:23:49 +0700

    Limit x/y padding to >= 1

---
+++ ls_Glass/core/modernize.lua
@@ -0,0 +1,28 @@
+local _, ns = ...
+local E, C, D, L = ns.E, ns.C, ns.D, ns.L
+
+-- Lua
+local _G = getfenv(0)
+local m_max = _G.math.max
+
+-- Mine
+function E:Modernize(data, name, key)
+	if not data.version then return end
+
+	if key == "profile" then
+		--> 100000.03
+		if data.version < 10000003 then
+			if data.chat then
+				if data.chat.x_padding then
+					data.chat.x_padding = m_max(1, data.chat.x_padding)
+				end
+
+				if data.chat.y_padding then
+					data.chat.y_padding = m_max(1, data.chat.y_padding)
+				end
+			end
+
+			data.version = 10000003
+		end
+	end
+end
