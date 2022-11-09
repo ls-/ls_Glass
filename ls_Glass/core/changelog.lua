e6aff438854c20c939a57aaa61725c4cb7893927
Val Voronov <i.lightspark@gmail.com>
2022-11-10 02:31:20 +0700

    📜

--- ls_Glass/core/changelog.lua
+++ ls_Glass/core/changelog.lua
@@ -6,5 +6,10 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- The initial release.
+- Added horizontal and vertical padding options for messages.
+- Fixed an issue where messages' background gradient wouldn't resize properly.
+- Fixed an issue where fast forwarding would break the message display.
+- Reduced the minimal possible chat frame size to 176x64, making it smaller will break stuff.
+  The max is uncapped.
+- Added French translation. Translated by Braincell1980@Curse.
 ]]
