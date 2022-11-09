825c92bf8ea135a47d1a1800553ffb74e8473b86
Val Voronov <i.lightspark@gmail.com>
2022-11-10 00:17:21 +0700

    👅

--- ls_Glass/locales/frFR.lua
+++ ls_Glass/locales/frFR.lua
@@ -1,4 +1,4 @@
--- Contributors:
+-- Contributors: Braincell1980@Curse
 
 local _, ns = ...
 local L = ns.L
@@ -7,3 +7,27 @@ local L = ns.L
 local _G = getfenv(0)
 
 if GetLocale() ~= "frFR" then return end
+
+L["BACKGROUND_ALPHA"] = "Opacité du fond"
+L["CHANGELOG"] = "Liste des changements "
+L["CHANGELOG_FULL"] = "Tout"
+L["CONFIG_WARNING"] = "Je recommande fortement de |cffffd200/reload|r l'interface utilisateur une fois que vous avez terminé de configurer l'addon ou même d'ouvrir ce panneau pour éviter toute erreur pendant un combat."
+L["DOCK_AND_EDITBOX"] = "Onglets et Zone d'édition"
+L["DOWNLOADS"] = "Téléchargements"
+L["FADE_IN_DURATION"] = "Durée du Fondu entrant"
+L["FADE_OUT_DELAY"] = "Délais du Fondu sortant"
+L["FADE_OUT_DURATION"] = "Durée du Fondu sortant"
+L["FADING"] = "Fondu"
+L["FONT"] = "Police"
+L["FONT_EDITBOX"] = "Police de la Zone d'édition"
+L["JUMP_TO_PRESENT"] = "Passer au présent"
+L["MESSAGES"] = "Messages"
+L["MOUSEOVER_TOOLTIPS"] = "Infobulle au survol de la souris"
+L["OPEN_CONFIG"] = "Ouvrir la configuration"
+L["OUTLINE"] = "Contour"
+L["PERSISTENT"] = "Persistent"
+L["SHADOW"] = "Ombre"
+L["SIZE"] = "Taille"
+L["SLIDE_IN_DURATION"] = "Durée d'apparition"
+L["SUPPORT"] = "Assistance"
+L["UNREAD_MESSAGES"] = "|TInterface\\CHATFRAME\\UI-ChatWhisperIcon:0:|t Messages non lu"
