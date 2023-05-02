dfb07b5f83685a6397ac3f88852cf0c553b83ea5
Val Voronov <i.lightspark@gmail.com>
2023-05-03 02:49:16 +0700

    📜

--- ls_Glass/core/changelog.lua
+++ ls_Glass/core/changelog.lua
@@ -6,8 +6,7 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Added 10.0.5 support.
-- Added optional buttons for scrolling up and down. It's an accessibility feature meant for users
-  who can't use the mouse wheel. Holding the Ctrl key will slow the scrolling rate by two times.
-- Added an option to fade in chat messages on mouseover.
+## Version 100100.01
+
+- Added 10.1.0 support.
 ]]
