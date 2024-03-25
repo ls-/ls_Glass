f6748c921d5d1625185d7de17c9fde566ba5c056
Val Voronov <i.lightspark@gmail.com>
2024-03-25 16:46:56 +0700

    📜

--- ls_Glass/core/changelog.lua
+++ ls_Glass/core/changelog.lua
@@ -7,4 +7,11 @@ local _G = getfenv(0)
 -- Mine
 E.CHANGELOG = [[
 - Added 10.2.6 support.
+- Fixed an issue where the chat frame would disappear after it's previously hidden.
+- Fixed an issue where some messages would appear truncated.
+- Fixed an issue where some characters would appear as squares. The addon now properly mimics the default chat's
+  behaviour which also includes all the bugs that come with it. For instance, depending on the alphabet those characters
+  belong to, those messages will completely ignore various chat settings like font size, outline, etc. It is this way 
+  due to limitation on Blizz end.
+- Set minimal Y padding to 0. Yes, no more gaps in the TomCats' messages :>
 ]]
