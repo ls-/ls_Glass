94544bb10b5487fa1b3102f2f9d670d9be69d23f
Val Voronov <i.lightspark@gmail.com>
2025-08-24 21:18:37 +0700

    📜

--- ls_Glass/core/changelog.lua
+++ ls_Glass/core/changelog.lua
@@ -6,9 +6,6 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Added 11.2.0 support.
-- Added an option not to use the Alt key for cursor movement inside the edit box.
-- Fixed an issue where the pet battle chat tab would stay nameless. It's a Blizz bug, but it's possible to add a workaround on my end.
-- Made scrolling/sliding animations smoother.
-- Removed the fade-in effect on new messages. It looked cool on oneliners, but on anything else it's pretty jarring. Whenever multiple new messages were sliding in, at first you'd see a large empty space, and then the text would appear. It's particularly bad, if the messages were also long. Sorry, but I couldn't stand it anymore. RIP Bozo.
+- Fixed an issue with the line spacing. There seems to be a Blizz bug where it just ends up increasing on its own.
+- Reworked and brought back the new message fade-in effect. Instead of fading in all new messages, only the last/lowest new messaged will be faded in. It should eliminate any weird emptiness while keeping things smooth. Bozo, welcome back.
 ]]
